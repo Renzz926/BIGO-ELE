d6c1f68f7d44f251f1c98a3dd27cacca9b138b65
Renzz <49543146+Renzz926@users.noreply.github.com>
2025-10-29 22:54:40 +0800

    fix

--- src/views/h5/index.vue
+++ src/views/h5/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>123</div>
+  <div class="content">123</div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style module scoped>
-.wrapper {
-  width: 100%;
+<style lang="scss" scoped>
+.content {
+  font-size: 20px;
 }
 </style>
